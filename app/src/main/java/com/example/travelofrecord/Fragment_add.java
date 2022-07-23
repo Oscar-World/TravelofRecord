c5200cfaa3bbd8fa53223adadf815de66f6ec1b5
Oscar <rkdgjdl@naver.com>
2022-07-23 14:55:07 +0900

    add image to fragment_add

--- app/src/main/java/com/example/travelofrecord/Fragment_add.java
+++ app/src/main/java/com/example/travelofrecord/Fragment_add.java
@@ -10,10 +10,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Fragment_add extends Fragment {
 
     String TAG = "추가 프래그먼트";
+
+    View v;
+
+    Button addUpload_Btn;
+    EditText addWrote_Edit;
+    ImageView addImage;
+
+    String wroteContent;
 
     @Override public void onAttach(Context context) {
         super.onAttach(context);
@@ -28,8 +39,11 @@ public class Fragment_add extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        v = inflater.inflate(R.layout.fragment_add, container, false);
+
+        setView();
+
+        return v;
     }
 
 
@@ -44,6 +58,32 @@ public class Fragment_add extends Fragment {
     @Override public void onStart() {
         Log.d(TAG, "onStart()");
         super.onStart();
+
+
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+
+        addUpload_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                wroteContent = addWrote_Edit.getText().toString();
+
+                
+
+            }
+        });
+
+
+
+
     }
     @Override public void onResume() {
         Log.d(TAG, "onResume()");
@@ -64,6 +104,14 @@ public class Fragment_add extends Fragment {
     @Override public void onDetach() {
         Log.d(TAG, "onDetach()");
         super.onDetach();
+    }
+
+    public void setView() {
+
+        addUpload_Btn = v.findViewById(R.id.addUpload_Btn);
+        addWrote_Edit = v.findViewById(R.id.add_Wrote);
+        addImage = v.findViewById(R.id.addImage_Btn);
+
     }
 
 }
